81dfbb6d1e5e415f63592ad7af4b01482ccd5ce8
tochisuke221 <tochisuke221@gmail.com>
2023-07-19 23:32:28 +0900

    React Hook Formの導入

---
+++ next-js-gihyo-book/src/pages/checkbox.tsx
@@ -0,0 +1,31 @@
+import { useForm, Controller, SubmitHandler } from 'react-hook-form'
+
+type MyFormData = {
+  isChecked: boolean
+}
+
+// 後で消すので一旦App
+const Checkbox = () => {
+  const { handleSubmit, control, formState: { errors } } = useForm<MyFormData>()
+  const onSubmit: SubmitHandler<MyFormData> = data => {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="isChecked"
+        control={control}
+        defaultValue={false}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <input type="checkbox" onChange={onChange} checked={value} />
+        )}
+      />
+      {errors.isChecked && <label>チェックしてください</label>}
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Checkbox
---
+++ next-js-gihyo-book/src/pages/input.tsx
@@ -0,0 +1,33 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type MyFormData = {
+  firstName: string
+  lastName: string
+  category: string
+}
+
+// 後で消すので一旦App
+const Input = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<MyFormData>()
+  const onSubmit: SubmitHandler<MyFormData> = data => {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName', { required: true })} placeholder="名前"></input>
+        {errors.firstName && <div>名前を入力してください</div>}
+        <input {...register('lastName', { required: true })} placeholder="苗字"></input>
+        {errors.lastName && <div>苗字を入力してください</div>}
+        <select  {...register('category', { required: true })}>
+            <option value="">選択...</option>
+            <option value="A">カテゴリA</option>
+            <option value="B">カテゴリB</option>
+        </select>
+        {errors.category && <div>カテゴリを選択してください</div>}
+        <input type="submit"></input>
+    </form>
+  )
+}
+
+export default Input

c967d7beab8579e38cc2cc96faafefbdcb0abdc3
KousukeTochikawa <tochikawa.kousuke@sumasate.jp>
2023-07-19 22:53:38 +0900

    storybookの導入

---
+++ next-js-gihyo-book/src/themes/fontSize.ts
@@ -0,0 +1,10 @@
+const fontSize: any = [12, 14, 16, 20, 24, 32]
+
+fontSize.extraSmall = fontSize[0]
+fontSize.small = fontSize[1]
+fontSize.medium = fontSize[2]
+fontSize.mediumLarge = fontSize[3]
+fontSize.large = fontSize[4]
+fontSize.extraLarge = fontSize[5]
+
+export default fontSize

9a68e27d59c85b57eb4e6ea9e4fb63f905ea7acd
tochisuke221 <tochisuke221@gmail.com>
2023-07-20 23:29:39 +0900

    Material Iconsの導入

--- next-js-gihyo-book/.storybook/main.js
+++ next-js-gihyo-book/.storybook/main.js
@@ -1,34 +1,28 @@
-import TsconfigPathsPlugin = require('tconfig-paths-webpack-plugin');
-const path = require('path')
-
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 module.exports = {
-  stories: [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ['public'],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  babel: async options => ({
-    ...options,
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private-methods',
-      '@babel/plugin-proposal-private-property-in-object',
-    ],
-    webpackFinal: async (config) => {
-      config.resolve.plugins = [
-        new TsconfigPathsPlugin({
-          configFile: path.resolve(__dirname, '../tsconfig.json')
-        })
-      ];
-
-      return config
-    }
-  })
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss", "@storybook/addon-onboarding", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  babel: async (options) => {
+    return {
+      ...options,
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-proposal-private-property-in-object',
+      ],
+    };
+  },
+  webpackFinal: async (config) => {
+    // Webpackの設定を追加する処理
+    return config;
+  },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };

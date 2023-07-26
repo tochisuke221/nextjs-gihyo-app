eb1e8a9c2ae7fc982a24fc0047056967dcb135de
tochisuke221 <tochisuke221@gmail.com>
2023-07-26 19:36:08 +0900

    その他コンポーネントの作成

---
+++ next-js-gihyo-book/src/components/atoms/Badge/index.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+// バッジの円形
+const BadgeWrapper = styled.div<{ backgroundColor: string }>`
+  border-radius: 20px;
+  height: 20px;
+  min-width: 20px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`
+
+// バッジ内のテキスト
+const BadgeText = styled.p`
+  color: white;
+  font-size: 11px;
+  user-select: none;
+`
+
+interface BadgeProps {
+  content: string
+  backgroundColor: string
+}
+
+/**
+ * バッジ
+ */
+const Badge = ({ content, backgroundColor }: BadgeProps) => {
+  return (
+    <BadgeWrapper backgroundColor={backgroundColor}>
+      <BadgeText>{content}</BadgeText>
+    </BadgeWrapper>
+  )
+}
+
+export default Badge

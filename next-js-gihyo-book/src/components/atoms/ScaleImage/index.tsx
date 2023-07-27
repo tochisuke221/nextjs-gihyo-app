1c3321ad4193ecd4554ab708df1531a5d11a0f2b
KousukeTochikawa <tochikawa.kousuke@sumasate.jp>
2023-07-27 19:38:02 +0900

    その他コンポーネント2

--- next-js-gihyo-book/src/components/atoms/ScaleImage/index.tsx
+++ next-js-gihyo-book/src/components/atoms/ScaleImage/index.tsx
@@ -1,6 +1,6 @@
 import Image, { ImageProps } from 'next/image'
 import styled from 'styled-components'
-import { Responsive } from '@/types/data'
+import { Responsive } from '@/types/styles'
 import { toPropValue } from '@/utils/styles'
 
 type ScaleImageProps =
